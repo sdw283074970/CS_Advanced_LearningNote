dcace3c7bbb078349b3c72626ce808259001faec
sdw283074970 <downwes@gmail.com>
2017-11-09 16:37:00 -0500

    Update Generics.cs

--- Generics.cs
+++ Generics.cs
@@ -1,7 +1,7 @@
-//F: 什么是泛型(Generics)?
+//Q: 什么是泛型(Generics)?
 //A: 直接解释过于抽象，不如先理清泛型的作用，以及了解泛型被设计来是为了解决什么样的问题。
 
-//F: 泛型有什么用？
+//Q: 泛型有什么用？
 //A: 泛型最开始要解决的是列表参数类型单一导致的实际运行效率低的问题。
 //如在Arraylist中，储存的元素类型统一为object。类型object是一切对象的基类，如int, string等常见类型都是object的派生类。在实操作中，
 //各种类型都能被放入到Arraylist中，如:
@@ -26,7 +26,7 @@ int num = list[0];//从stack中取出
 
 //另外Stack<T>也是常用的泛型，不再赘述。
 
-//F: 那List<T>类内部长啥样？
+//Q: 那List<T>类/方法内部长啥样？
 //A: 以下展示仿照List<T>类及其中的Add()方法和取出值方法的构造:
 
 Public GenericList<T> //T为占位符，占位符是什么无所谓，约定俗成为T
@@ -42,8 +42,8 @@ Public GenericList<T> //T为占位符，占位符是什么无所谓，约定俗�
   //... 更多方法
 }
 
-//F: 可否根据需求定制自己想要的泛型类？
-//A: 完全可以。以上例子其实就是一种自己定制的泛型类。以下再加个自制的Dictionary<TKey, TValue>泛型类，有所不同的是这里有两个占位符:
+//Q: 可否根据需求定制自己想要的泛型类/方法？
+//A: 完全可以。以上例子其实就是一种自己定制的泛型类/方法。以下再加个自制的Dictionary<TKey, TValue>泛型类，有所不同的是这里有两个占位符:
 
 public GenericDictionary<TKey, TValue> //若有两个以上不同类型占位符，必须区别开来，通常为T+区分关键词
 {
@@ -57,13 +57,32 @@ public GenericDictionary<TKey, TValue> //若有两个以上不同类型占位符
   }
 }
 
-//以下为这个类的使用方法:
+//以下为这个类/方法的使用方法:
 
 var dic = new GenericDictionary<string, int>();
 dic.Add("name", 3);
 int value = dic["name"];
 
+//Q: 是否任何类型都能传递到占位符T上？
+//A: 除了抽象类，任何类都能传递进去。当然我们也可以为能传入进去的类型加上人工限制。
+//为什么要加入人工限制？当然是为了更好的发挥泛型作用。没有限制的泛型在编译的时候系统默认其为object类，也就是说只能使用最原始的object那几个方法。
+//在一些情况下我们并不需要特别宽泛的泛型，比如在一个方法中，我们只想要数字类型，具体如要对传入参数进行比较，我们知道所有数字类都可以比较，但是
+//系统并不知道我们只会传入long, int, float等数字类型的值。这个时候我们就要使用关键词Where加上人工限制。以这个例子为例:
 
+public class Untilites<T> where T : IComparable
+{
+  public T Max(T a, T b)
+  {
+    return a.CompareTo(b) > 0 ? a : b;
+  }
+}
+
+//意思为T类型为所有有IComparable接口的类。另外列出所有五种类型的限制：
+//1. 限制占位类为有该接口的类。如 Where T : IComparable(任何借口)
+//2. 限制占位类为某一特定类和该类的子类的类。如 Where T : Product(任何指定的类)
+//3. 限制占位类为值类型(Value type)。仅 Where T : struct
+//4. 限制占位类为引用类型(Reference type)。仅 Where T : class
+//5. 限制占位类为有默认构造器的类。仅 Where T : new()
 
 
 
